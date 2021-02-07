9ec58fab1f4a3cd06ad3b1da5c2d9574866526de
ktno <kittinan@atato.com>
2021-02-07 13:45:34 +0700

    Rotate Django SECRET_KEY in view/coding-with-mitch

--- todos/view/coding-with-mitch/project/project/settings.py
+++ todos/view/coding-with-mitch/project/project/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from environ import Env
+
+env = Env() 
+env.read_env(env_file='modelvideo/.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '91q+g%)9)4n$+5+)aiu@uxsn6v=j3l8n5^+q!$modw8n&9nay&'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
